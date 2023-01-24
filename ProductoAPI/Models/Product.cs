f13eb4ede0548a5b8c96bacde6cbcf68c82339ce
Ricar2Daza <Rikadaza11@hotmail.com>
2023-01-24 00:14:28 -0500

    Agregar archivos de proyecto.

---
+++ ProductoAPI/Models/Product.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProductAPI
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        
+        [StringLength(30)]
+        public string Name { get; set; }
+
+        [StringLength(20)]
+        public string Status { get; set; } = string.Empty;
+
+    }
+}
